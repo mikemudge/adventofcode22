05faa9b99bb7ebbaab7bc4deea6e8071357fd2a5
Michael <mike.mudge@gmail.com>
2022-12-23 13:05:49 +1300

    [MAIN] Solve part1 puzzle16

---
+++ puzzle16/solution.php
@@ -0,0 +1,216 @@
+<?php
+
+$lines = file(dirname(__FILE__) . "/input");
+//$lines = file(dirname(__FILE__) . "/sample");
+$part1 = 0;
+$part2 = 0;
+
+$lines = array_map("trim", $lines);
+
+$nodes = [];
+for($i = 0; $i < count($lines); $i++) {
+    $line = $lines[$i];
+//    echo("$line\n");
+    [$left, $right] = explode("; ", $line);
+    [$name,$rate] = explode(" has flow rate=", substr($left, 6));
+    $rate = intval($rate);
+    if (str_starts_with($right, "tunnels lead to valves ")) {
+        $valves = explode(", ", substr($right, 23));
+    } else {
+        $valves = explode(", ", substr($right, 22));
+    }
+    echo("Valve $name with rate=$rate connects to " . json_encode($valves) . PHP_EOL);
+    // Build a graph and find a path.
+    $nodes[$name] = [
+        "tunnels" => $valves,
+        "rate" => $rate
+    ];
+}
+
+// Calculate the maximum amount of pressure which can be released.
+$pressureRate = 0;
+foreach($nodes as $node) {
+    $pressureRate = $node['rate'];
+}
+
+// Translate into a new graph.
+// Use weighted edges to represent travel time between nodes.
+$nodes2 = [];
+function shortestPaths(array $nodes, string $start) {
+    $next = [$start];
+    $distances = [];
+    $distances[$start] = 0;
+
+    while(!empty($next)) {
+        $tmp = $next;
+        $next = [];
+        foreach ($tmp as $name) {
+            $tunnels = $nodes[$name]['tunnels'];
+            foreach ($tunnels as $tunnel) {
+                // If we don't already have a way to reach this, add one.
+                if (!array_key_exists($tunnel, $distances)) {
+                    $distances[$tunnel] = 1 + $distances[$name];
+                    // A new node was discovered, so use it next time.
+                    $next[] = $tunnel;
+                }
+            }
+        }
+    }
+
+    // Remove the path to yourself as its not helpful.
+    unset($distances[$start]);
+    return $distances;
+
+}
+
+foreach($nodes as $name => $node) {
+    // Only include nodes which have a value in enabling.
+    if ($name == "AA" || $node['rate'] > 0) {
+        echo("transforming $name\n");
+
+        $paths = shortestPaths($nodes, $name);
+        foreach($paths as $p=>$cost) {
+            // Don't consider paths to nodes with 0 rate.
+            if ($nodes[$p]['rate'] == 0) {
+                unset($paths[$p]);
+            }
+        }
+
+        $nodes2[$name] = [
+            'rate' => $node['rate'],
+            'paths' => $paths
+        ];
+
+    }
+}
+
+echo(json_encode($nodes2['AA']) . PHP_EOL);
+
+function solvePart1($nodes, $nodes2) {
+    // Initially at AA with 30 minutes, no pressure released and no nodes enabled.
+    $state = ["AA", 30, 0, []];
+    $pq = new SplPriorityQueue();
+    $pq->insert($state, 0);
+    $bestPressure = 0;
+    $bestVisited = [];
+    while($pq->valid()) {
+        $state = $pq->extract();
+        [$name, $timeRemaining, $pressure, $visited] = $state;
+        if ($pressure > $bestPressure) {
+            $bestVisited = $visited;
+        }
+        $bestPressure = max($pressure, $bestPressure);
+        foreach ($nodes2[$name]['paths'] as $p=>$cost) {
+            if (array_key_exists($p, $visited)) {
+                // Can't reenable this node.
+                continue;
+            }
+            $rate = $nodes[$p]['rate'];
+            if ($rate == 0) {
+                // No point in activating this node.
+                continue;
+            }
+            // travel time + open valve time.
+            $dt = $cost + 1;
+            if ($dt > $timeRemaining) {
+                // Its not possible to make it and enable in the time remaining.
+                continue;
+            }
+            // Turning on the value at time $time + $dt will release pressure.
+            // Calculate how much pressure it releases given the remaining time.
+            $dp = ($timeRemaining - $dt) * $rate;
+            $newvisited = [];
+            $newvisited[$p] = $timeRemaining - $dt;
+            foreach ($visited as $v=>$c) {
+                $newvisited[$v] = $c;
+            }
+            $pq->insert([$p, $timeRemaining - $dt, $pressure + $dp, $newvisited], 0);
+        }
+    }
+
+    echo(json_encode($bestVisited) . PHP_EOL);
+    return $bestPressure;
+}
+
+function solvePart2($nodes, $nodes2) {
+    // Initially boath at AA with 26 minutes, no pressure released and no nodes enabled.
+    $state = ["AA", "AA", 26, 26, 0, []];
+    $pq = new SplPriorityQueue();
+    $pq->insert($state, 0);
+    $bestPressure = 0;
+    $bestVisited = [];
+    while($pq->valid()) {
+        $state = $pq->extract();
+        [$name, $eleName, $timeRemaining, $eleTimeRemaining, $pressure, $visited] = $state;
+        if ($pressure > $bestPressure) {
+            $bestVisited = $visited;
+        }
+//        echo(json_encode($state) . PHP_EOL);
+        $bestPressure = max($pressure, $bestPressure);
+        if ($timeRemaining > $eleTimeRemaining) {
+            foreach ($nodes2[$name]['paths'] as $p => $cost) {
+                if (array_key_exists($p, $visited)) {
+                    // Can't reenable this node.
+                    continue;
+                }
+                $rate = $nodes[$p]['rate'];
+                if ($rate == 0) {
+                    // No point in activating this node.
+                    continue;
+                }
+                // travel time + open valve time.
+                $dt = $cost + 1;
+                if ($dt > $timeRemaining) {
+                    // Its not possible to make it and enable in the time remaining.
+                    continue;
+                }
+                // Turning on the value at time $time + $dt will release pressure.
+                // Calculate how much pressure it releases given the remaining time.
+                $dp = ($timeRemaining - $dt) * $rate;
+                $newvisited = [];
+                $newvisited[$p] = $timeRemaining - $dt;
+                foreach ($visited as $v => $c) {
+                    $newvisited[$v] = $c;
+                }
+                $pq->insert([$p, $eleName, $timeRemaining - $dt, $eleTimeRemaining, $pressure + $dp, $newvisited], 0);
+            }
+        } else {
+            // Consider moves for the elephant
+            foreach ($nodes2[$eleName]['paths'] as $p => $cost) {
+                if (array_key_exists($p, $visited)) {
+                    // Can't reenable this node.
+                    continue;
+                }
+                $rate = $nodes[$p]['rate'];
+                if ($rate == 0) {
+                    // No point in activating this node.
+                    continue;
+                }
+                // travel time + open valve time.
+                $dt = $cost + 1;
+                if ($dt > $eleTimeRemaining) {
+                    // Its not possible to make it and enable in the time remaining.
+                    continue;
+                }
+                // Turning on the value at time $time + $dt will release pressure.
+                // Calculate how much pressure it releases given the remaining time.
+                $dp = ($eleTimeRemaining - $dt) * $rate;
+                $newvisited = [];
+                $newvisited[$p] = $eleTimeRemaining - $dt;
+                foreach ($visited as $v => $c) {
+                    $newvisited[$v] = $c;
+                }
+                $pq->insert([$name, $p, $timeRemaining, $eleTimeRemaining - $dt, $pressure + $dp, $newvisited], 0);
+            }
+        }
+    }
+
+    echo(json_encode($bestVisited) . PHP_EOL);
+    return $bestPressure;
+}
+
+$part1 = solvePart1($nodes, $nodes2);
+echo "Part 1: $part1\n";
+
+$part2 = solvePart2($nodes, $nodes2);
+echo "Part 2: $part2\n";
